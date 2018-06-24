093d2e77bc5311ae9f06cadc8fd78f93cf936bbf
lijieaa <952054767@qq.com>
2018-06-24 22:29:26 +0800

    Initial commit

---
+++ src/test/java/com/jpm/jsse/TestKeyStore.java
@@ -0,0 +1,17 @@
+package com.jpm.jsse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestKeyStore {
+
+    @Before
+    public void init(){
+        System.out.println("init");
+    }
+
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+}
